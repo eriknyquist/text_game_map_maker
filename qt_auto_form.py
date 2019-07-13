b7197481ceb9d5cfca8ef4fea75cdd5e219bd7b6
Erik <eknyquist@gmail.com>
2019-07-12 21:47:12 -0700

    Updates

--- qt_auto_form.py
+++ qt_auto_form.py
@@ -1,43 +1,49 @@
-from PyQt5.QtWidgets import (QApplication, QComboBox, QDialog,
-QDialogButtonBox, QFormLayout, QGridLayout, QGroupBox, QHBoxLayout,
-QLabel, QLineEdit, QMenu, QMenuBar, QPushButton, QSpinBox, QDoubleSpinBox,
-QTextEdit, QVBoxLayout)
+from PyQt5.QtWidgets import (QApplication, QDialog, QDialogButtonBox,
+    QFormLayout, QGridLayout, QGroupBox, QHBoxLayout, QLabel, QLineEdit,
+    QSpinBox, QDoubleSpinBox, QTextEdit, QVBoxLayout, QCheckBox)
 
 import sys
 
 _input_decoders = {
-    'str': (lambda x: x.text(), lambda: QLineEdit()),
-    'int': (lambda x: x.value(), lambda: QSpinBox()),
-    'float': (lambda x: x.value(), lambda: QDoubleSpinBox())
+    'str': (lambda x: x.text(), lambda x, y: x.setText(y), lambda: QLineEdit()),
+    'int': (lambda x: x.value(), lambda x, y: x.setValue(y), lambda: QSpinBox()),
+    'float': (lambda x: x.value(), lambda x, y: x.setValue(y), lambda: QDoubleSpinBox()),
+    'bool': (lambda x: x.isChecked(), lambda x, y: x.setChecked(y), lambda: QCheckBox())
 }
 
 class InputWidget(object):
-    def __init__(self, instance, attr, input_getter, widget_getter):
+    def __init__(self, instance, attr, value_getter, value_setter, widget_getter):
         self.instance = instance
         self.attr = attr
-        self.input_getter = input_getter
+        self.value_getter = value_getter
+        self.value_setter = value_setter
         self.widget = widget_getter()
 
-    def writeValue(self):
-        value = self.input_getter(self.widget)
+    def setInstanceValue(self):
+        value = self.value_getter(self.widget)
         setattr(self.instance, self.attr, value)
+
+    def setWidgetValue(self, value):
+        value = getattr(self.instance, self.attr)
+        self.value_setter(self.widget, value)
 
     def rowItems(self):
         return (QLabel("%s:" % self.attr), self.widget)
 
 class TestClass(object):
     def __init__(self):
-        self.intval = 0
-        self.strval = ""
-        self.floatval = 0.0
+        self.intval = 26
+        self.strval = "ditpojid"
+        self.floatval = 0.454
+        self.boolval = False
 
 def getInputWidgetForAttr(instance, attrname):
     typestr = type(getattr(instance, attrname)).__name__
     if typestr not in _input_decoders:
         typestr = 'str'
 
-    input_getter, widget_getter = _input_decoders[typestr]
-    return InputWidget(instance, attrname, input_getter, widget_getter)
+    value_getter, value_setter, widget_getter = _input_decoders[typestr]
+    return InputWidget(instance, attrname, value_getter, value_setter, widget_getter)
 
 class Dialog(QDialog):
     NumGridRows = 3
@@ -62,7 +68,7 @@ class Dialog(QDialog):
 
     def writeInstanceValues(self):
         for widget in self.widgets:
-            widget.writeValue()
+            widget.setInstanceValue()
 
     def createFormFromInstance(self, instance):
         classname = instance.__class__.__name__
@@ -71,8 +77,8 @@ class Dialog(QDialog):
 
         self.widgets = []
         for attrname in instance.__dict__:
-            attr = instance.__dict__[attrname]
             input_widget = getInputWidgetForAttr(instance, attrname)
+            input_widget.setWidgetValue(instance.__dict__[attrname])
             layout.addRow(*input_widget.rowItems())
             self.widgets.append(input_widget)
 
@@ -84,4 +90,4 @@ if __name__ == '__main__':
     dialog = Dialog(ins)
     dialog.exec_()
 
-    print("intval=%d, floatval=%.2f, strval=%s" % (ins.intval, ins.floatval, ins.strval))
+    print("intval=%d, floatval=%.2f, strval=%s, boolval=%s" % (ins.intval, ins.floatval, ins.strval, ins.boolval))
