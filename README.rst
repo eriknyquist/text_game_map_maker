db653eb2d91048dfbcfdf8d3f0ce9519bbfaec39
Erik <eknyquist@gmail.com>
2019-08-19 22:30:02 -0700

    make it a proper python package

---
+++ README.rst
@@ -0,0 +1,12 @@
+Graphical map builder for text_game_maker
+=========================================
+
+Install with pip:
+
+::
+    pip install text_game_map_maker
+
+And run it via the Python interpreter:
+
+::
+    python3 -m text_game_map_maker
---
+++ setup.py
@@ -0,0 +1,34 @@
+import unittest
+import os
+from setuptools import setup, find_packages
+from distutils.core import Command
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+README = os.path.join(HERE, "README.rst")
+REQFILE = 'requirements.txt'
+
+classifiers = [
+    'License :: OSI Approved :: Apache Software License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3',
+]
+
+with open(README, 'r') as f:
+    long_description = f.read()
+
+with open(REQFILE, 'r') as fh:
+    dependencies = fh.readlines()
+
+setup(
+    name='text_game_map_maker',
+    version='0.6.1',
+    description=('Graphical tool for building maps to be used with text_game_maker'),
+    long_description=long_description,
+    url='http://github.com/eriknyquist/text_game_map_maker',
+    author='Erik Nyquist',
+    author_email='eknyquist@gmail.com',
+    license='Apache 2.0',
+    install_requires=dependencies,
+    packages=['text_game_map_maker'],
+)
