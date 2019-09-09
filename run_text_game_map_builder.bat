96c6d8314f85a6afb6899982c3b6efca2eb36d3b
Erik Nyquist <eknyquist@gmail.com>
2019-09-08 17:53:40 -0700

    Update windows scripts

--- run_text_game_map_builder.bat
+++ run_text_game_map_builder.bat
@@ -1,1 +1,11 @@
+@echo off
+
 start pythonw -m text_game_map_maker
+IF ERRORLEVEL 0 (goto :end)
+
+start pyw -m text_game_map_maker
+IF ERRORLEVEL 0 (goto :end)
+
+python -m text_game_map_maker
+
+:end
---
+++ update_text_game_map_builder.bat
@@ -0,0 +1,6 @@
+@echo off
+
+python -m pip install text_game_map_maker --upgrade
+
+echo Press any key to exit
+pause >nul
