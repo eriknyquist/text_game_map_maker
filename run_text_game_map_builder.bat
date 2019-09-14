4312805feb8a293b672764ae3ea3bbc92c2b538d
Erik <eknyquist@gmail.com>
2019-09-14 12:29:10 -0700

    Update run_text_game_map_builder.bat

--- run_text_game_map_builder.bat
+++ run_text_game_map_builder.bat
@@ -3,7 +3,7 @@
 start pythonw -m text_game_map_maker
 IF ERRORLEVEL 0 (goto :end)
 
-start pyw -m text_game_map_maker
+start pyw -3 -m text_game_map_maker
 IF ERRORLEVEL 0 (goto :end)
 
 python -m text_game_map_maker
